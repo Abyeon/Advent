a8bd48a7b46f06823c5716d8e311eeb44eba52e8
Abyeon <wpseals@gmail.com>
2025-12-02 00:55:05 -0500

    Add solution to day two

---
+++ Advent/Lib/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Advent.Lib;
+
+public static class StringExtensions
+{
+    public static IEnumerable<string> SplitInParts(this string s, int partLength) {
+        ArgumentNullException.ThrowIfNull(s);
+        
+        if (partLength <= 0)
+            throw new ArgumentException("Part length has to be positive.", nameof(partLength));
+
+        for (var i = 0; i < s.Length; i += partLength)
+            yield return s.Substring(i, Math.Min(partLength, s.Length - i));
+    }
+}
---
+++ Advent/Solutions/2025/2/GiftShop.cs
@@ -0,0 +1,70 @@
+﻿using Advent.Lib;
+
+namespace Advent.Solutions._2025._2;
+
+public class GiftShop : Solution
+{
+    public void PartOne(string[] input)
+    {
+        var lines = input[0].Split(',');
+        
+        long total = 0;
+        foreach (var line in lines)
+        {
+            var min = long.Parse(line.Split('-')[0]);
+            var max = long.Parse(line.Split('-')[1]);
+
+            for (var i = min; i <= max; i++)
+            {
+                var current = i.ToString();
+
+                var firstHalf = current[..(current.Length / 2)];
+                var secondHalf = current[(current.Length / 2)..];
+                if (firstHalf != secondHalf) continue;
+                
+                total += long.Parse(current);
+            }
+        }
+        
+        Console.WriteLine($"Total is: {total}");
+    }
+
+    private static bool AllEqual<T>(T[] array)
+    {
+        if (array is not { Length: > 1 })
+        {
+            return true;
+        }
+
+        var firstElement = array[0];
+        return array.All(element => EqualityComparer<T>.Default.Equals(element, firstElement));
+    }
+    
+    public void PartTwo(string[] input)
+    {
+        var lines = input[0].Split(',');
+        
+        long total = 0;
+        foreach (var line in lines)
+        {
+            var min = long.Parse(line.Split('-')[0]);
+            var max = long.Parse(line.Split('-')[1]);
+
+            for (var i = min; i <= max; i++)
+            {
+                var current = i.ToString();
+
+                for (var j = 1; j < current.Length; j++)
+                {
+                    var split = current.SplitInParts(j);
+                    if (!AllEqual(split.ToArray())) continue;
+                    
+                    total += long.Parse(current);
+                    break;
+                }
+            }
+        }
+        
+        Console.WriteLine($"Total is: {total}");
+    }
+}
